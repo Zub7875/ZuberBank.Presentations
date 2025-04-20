3efc7d92b715f18b0f2bbd858f57cac1ffddd41e
Zub7875 <shaikhzuberfd@gmail.com>
2025-04-20 20:50:08 +0530

    Added the Icustomer class Added the Customer Class with Implementation

--- ZuberBank.Entities/Contracts/ICustomer.cs
+++ ZuberBank.Entities/Contracts/ICustomer.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZuberBank.Entities
+namespace ZuberBank.Entities.Contracts
 {
-    public class ICustomer
+    public interface ICustomer
     {
+        Guid CustomerID { get; set; }
+
+        long CustomerCode {  get; set; }
+
+        string CustomerName { get; set; }
+
+        string Address {  get; set; }
+
+        string Landmark { get; set; }
+
+        string City { get; set; }
+
+        string Country { get; set; }
+
+        string Mobile { get; set; }
+
+
     }
 }
