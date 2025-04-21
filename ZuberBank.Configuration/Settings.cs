da6886b4db006a2351ed2ed117fab3418c79d87e
Zub7875 <shaikhzuberfd@gmail.com>
2025-04-21 09:58:31 +0530

    Added Configuration Settings class

---
+++ ZuberBank.Configuration/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZuberBank.Configuration
+{
+    /// <summary>
+    /// Project Level Configuration Settings
+    /// </summary>
+    public static class Settings
+    {
+        /// <summary>
+        /// Customer Number Starts From 1001 and increment by 1;
+        /// </summary>
+
+        public static long BaseCustomerNumber { get; set; } = 1000;
+    }
+}
